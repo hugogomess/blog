444c95dc822f229e2e051c0f36f143051db6c13c
Hugo <201710074@aluno.fjn.edu.br>
2019-03-18 00:18:15 -0300

    add home

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+require('../models/Post');
+const Post = mongoose.model('Post');
 
 router.get('/',(req, res) =>{
-    res.send('Homepage');
+    Post.find().populate('category').sort({date: 'desc'}).then((posts) => {
+        
+        res.render('index', {posts: posts});
+
+    }).catch((err) => {
+        
+        req.flash('errorMsg', 'Ocorreu um erro interno!');
+        res.redirect('/404');
+
+    }); 
+});
+
+router.get('/404',(req, res) => {
+    res.send('Error 404 :(');
 });
 
 module.exports = router;
