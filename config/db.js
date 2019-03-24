36d64ce7621279713dcddeb869818702fd819db6
Hugo <hugogomes02@gmail.com>
2019-03-24 03:06:05 -0300

    update mongodb config

--- config/db.js
+++ config/db.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
 
+var mongoURI;
+
+if (process.env.NODE_ENV == 'production'){
+    mongoURI = 'mongodb+srv://12345:12345@blogapp-prod-cua5t.mongodb.net/test?retryWrites=true';
+} else {
+    mongoURI = 'mongodb://localhost:27017/blog';
+}
+
 //connect to mongodb
-mongoose.connect('mongodb://localhost:27017/blog', { useNewUrlParser: true}).then(() => {
+mongoose.connect(mongoURI, { useNewUrlParser: true}).then(() => {
     console.log('Connected to mongodb!');
 }).catch((err) => {
     console.log('Error to connect with mongodb: ' + err);
